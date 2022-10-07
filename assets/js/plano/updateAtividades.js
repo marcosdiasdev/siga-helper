6a3bab349e550c70cd6a60c78e79cf901aa234bd
Marcos Dias <ti.marcosdias@gmail.com>
2022-10-07 12:11:53 -0300

    Change var to const

--- assets/js/plano/updateAtividades.js
+++ assets/js/plano/updateAtividades.js
@@ -1,14 +1,7 @@
-/*
-var campoData = document.getElementById("formulario:passoAPasso:dataPrevista_input");
-var campoBase = document.getElementsByName("formulario:passoAPasso:j_idt95")[0];
-var insertBtn = document.getElementsByName("formulario:passoAPasso:j_idt97")[0];
-*/
+const painelDeAtividades = document.getElementById("formulario:passoAPasso:panelAtividades");
 
-var painelDeAtividades = document.getElementById("formulario:passoAPasso:panelAtividades");
-
-var camposData = painelDeAtividades.querySelectorAll("table tbody tr td:nth-of-type(1n) span input");
-var camposConteudo = painelDeAtividades.querySelectorAll("table tbody tr td:nth-of-type(2n) textarea");
-//Array.from(elementos).length;
+const camposData = painelDeAtividades.querySelectorAll("table tbody tr td:nth-of-type(1n) span input");
+const camposConteudo = painelDeAtividades.querySelectorAll("table tbody tr td:nth-of-type(2n) textarea");
 
 camposData.forEach(function(field, i) {
     field.value = atividades[i][0];
